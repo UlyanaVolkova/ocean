5b77f1ed83e35045ce48321c38a5e0dd35a459df
Ульяна <skipina_ulyana24@mail.ru>
2023-08-21 00:40:02 +0300

    Add set and get

--- src/main/java/ru/volkova/oceansimulation/entity/Shark.java
+++ src/main/java/ru/volkova/oceansimulation/entity/Shark.java
@@ -7,4 +7,32 @@ public class Shark {
     private int oxygen = 100;
     private double mouth = 3.3;
 
+
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public void setEnergy(int energy){
+        this.energy = energy;
+    }
+
+    public int getEnergy(){
+        return this.energy;
+    }
+
+    public void setOxygen(int oxygen){
+        this.oxygen = oxygen;
+    }
+
+    public int getOxygen(){
+        return this.oxygen;
+    }
+
+    public double getMouth() {
+        return this.mouth;
+    }
 }
