b0ccb81bbb2250ad9f47da22afd2afe23af9b271
Ульяна <skipina_ulyana24@mail.ru>
2023-08-21 01:27:47 +0300

    Add oxygen

--- src/main/java/ru/volkova/oceansimulation/util/EventProducer.java
+++ src/main/java/ru/volkova/oceansimulation/util/EventProducer.java
@@ -10,20 +10,33 @@ public class EventProducer {
 
     private void sleepEvent(Shark shark){
         int energy = shark.getEnergy();
+        int oxygen = shark.getOxygen();
         energy = energy +20;
         if(energy > 100){
             energy = 100;
         }
+        oxygen = oxygen - 5;
+        if(oxygen < 0){
+            oxygen = 0;
+        }
         shark.setEnergy(energy);
-        System.out.println("Акула вздремнула! + 20 энергии. Текущая энергия " + shark.getEnergy());
+        shark.setOxygen(oxygen);
+        System.out.println("Акула вздремнула! + 20 энергии, - 5 кислорода. Текущая энергия " + shark.getEnergy() + ". Текущий уровень кислорода " + shark.getOxygen());
     }
 
     private void swimming(Shark shark){
         int energy = shark.getEnergy();
+        int oxygen = shark.getOxygen();
         energy = energy - 5;
         if(energy<0){
             energy=0;
         }
+        oxygen = oxygen - 5;
+        if(oxygen < 0){
+            oxygen = 0;
+        }
         shark.setEnergy(energy);
+        shark.setOxygen(oxygen);
+        System.out.println("Акула плавала в океане, но никого не встретила( - 5 энергии и - 5 кислорода. Текущая енергия " + shark.getEnergy() + ". Текущий уровень кислорода " + shark.getOxygen());
     }
 }
