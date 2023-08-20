1b51084cae7ac4e3b5519d67078c4f2bc99f5395
Ульяна <skipina_ulyana24@mail.ru>
2023-08-21 00:26:28 +0300

    Add model

---
+++ src/main/java/ru/volkova/oceansimulation/_main/_Main.java
@@ -0,0 +1,8 @@
+package ru.volkova.oceansimulation._main;
+
+public class _Main {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/ru/volkova/oceansimulation/entity/Shark.java
@@ -0,0 +1,10 @@
+package ru.volkova.oceansimulation.entity;
+
+public class Shark {
+
+    private int health =100;
+    private int energy = 100;
+    private int oxygen = 100;
+    private double mouth = 3.3;
+
+}
