2d343b4ce6806690cdbaba454f4efee626ccf10c
Ульяна <skipina_ulyana24@mail.ru>
2023-08-21 23:57:41 +0300

    Add eat

--- src/main/java/ru/volkova/oceansimulation/util/EventProducer.java
+++ src/main/java/ru/volkova/oceansimulation/util/EventProducer.java
@@ -39,4 +39,48 @@ public class EventProducer {
         shark.setOxygen(oxygen);
         System.out.println("Акула плавала в океане, но никого не встретила( - 5 энергии и - 5 кислорода. Текущая енергия " + shark.getEnergy() + ". Текущий уровень кислорода " + shark.getOxygen());
     }
+
+    private void eatCrab(Shark shark){
+        int energy = shark.getEnergy();
+        int health = shark.getHealth();
+        int oxygen = shark.getOxygen();
+        energy = energy - 5;
+        if(energy<0){
+            energy=0;
+        }
+        health = health + (int)(shark.getMouth()*3);
+        if(health>100){
+            health=100;
+        }
+        oxygen = oxygen - 5;
+        if(oxygen < 0){
+            oxygen = 0;
+        }
+        shark.setEnergy(energy);
+        shark.setHealth(health);
+        shark.setOxygen(oxygen);
+        System.out.println("У акулы легкий перекус крабом. Текущая енергия " + shark.getEnergy() + ". Текущее здоровье " + shark.getHealth() + ". Текущий уровень кислорода " + shark.getOxygen());
+    }
+    private void eatTrout(Shark shark){
+        int energy = shark.getEnergy();
+        int health = shark.getHealth();
+        int oxygen = shark.getOxygen();
+        energy = energy - 7;
+        if(energy<0){
+            energy=0;
+        }
+        health = health + (int)(shark.getMouth()*6);
+        if(health>100){
+            health=100;
+        }
+        oxygen = oxygen - 5;
+        if(oxygen < 0){
+            oxygen = 0;
+        }
+        shark.setEnergy(energy);
+        shark.setHealth(health);
+        shark.setOxygen(oxygen);
+        System.out.println("Охота удалась! Акула съела форель. Текущая енергия " + shark.getEnergy() + ". Текущее здоровье " + shark.getHealth() + ". Текущий уровень кислорода " + shark.getOxygen());
+    }
+
 }
